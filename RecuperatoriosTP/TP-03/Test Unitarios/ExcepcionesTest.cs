482c15da3d195e16f0a3d41cd0114325844e9c74
Tomas prro <alcaine37@gmail.com>
2020-11-28 18:36:32 -0300

    Recuperatorio tp

---
+++ RecuperatoriosTP/TP-03/Test Unitarios/ExcepcionesTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntidadesAbstractas;
+using EntidadesInstanciadas;
+using Excepciones;
+
+
+namespace Test_Unitarios
+{
+    [TestClass]
+    public class ExcepcionesTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(AlumnoRepetidoException))]
+        public void Test_AlumnoRepetidoException()
+        {
+            Universidad u = new Universidad();
+            Alumno a = new Alumno(1694, "Tomas", "Alcaine", "41668879", Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion, Alumno.EEstadoCuenta.AlDia);
+
+            u += a;
+            u += a;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DniInvalidoException))]
+        public void Test_DniInvalidoException()
+        {
+            Alumno a = new Alumno(1694, "Tomas", "Alcaine", "hola", Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion, Alumno.EEstadoCuenta.AlDia);
+            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NacionalidadInvalidaException))]
+        public void Test_NacionalidadInvalidaException()
+        {
+            Alumno a = new Alumno(1694, "Tomas", "Alcaine", "90000000", Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion, Alumno.EEstadoCuenta.AlDia);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(SinProfesorException))]
+        public void TestSinProfesor()
+        {
+            Universidad universidadAux = new Universidad();
+            universidadAux += Universidad.EClases.Laboratorio;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArchivoException))]
+        public void TestArchivoInvalido()
+        {
+            Texto archivoDeTexto = new Texto();
+            string path = AppDomain.CurrentDomain.BaseDirectory + "Jornada.txt";
+            archivoDeTexto.Leer(path, out string datosLeidos);
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP-03/Test Unitarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test Unitarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test Unitarios")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c376d4c-9eb3-4e4d-b6fa-4fc4edaa4c67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
